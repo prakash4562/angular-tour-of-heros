f9f6de5004dd1fedb9cb94f5b0dceb9131cb4020
chitranshoo <chitranshoo10081998@gmail.com>
2022-06-05 23:18:13 +0530

    committed

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,12 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 1, name: 'Dr. Nice' },
+  { id: 2, name: 'Bombasto' },
+  { id: 3, name: 'Celeritas' },
+  { id: 4, name: 'Magneta' },
+  { id: 5, name: 'RubberMan' },
+  { id: 6, name: 'Virat Kohli' },
+  { id: 7, name: 'Ronaldo' },
+  { id: 8, name: 'Messi' }
 ];
